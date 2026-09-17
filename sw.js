d01a05a52cac95e29038b424a264797c53533043
Lp22222 <lp279@hotmail.com>
2026-09-17 22:10:13 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "mis-fondos-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
